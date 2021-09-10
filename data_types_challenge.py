c9aa2a7f9b3ca66b964080363bd8f51427ac0923
zsherry2106 <zsherry2106@gmail.com>
2021-09-10 18:55:54 -0500

    data types challenge

---
+++ data_types_challenge.py
@@ -0,0 +1,41 @@
+#Sherry Zhang
+#9/10/21
+
+#Data Types Challenge
+
+#os does not work for me
+import os
+os.system('cls')
+
+#create variables with dfferent data types and print data type
+variable1 = "hello"
+variable2 = True
+variable3 = 19
+variable4 = 20.1
+
+print(variable1, "data type:", type(variable1))
+print(variable2, "data type:", type(variable2))
+print(variable3, "data type:", type(variable3))
+print(variable4, "data type:", type(variable4))
+
+#Calculate addition, subtraction, multiplication, and division of 2 variables of same type 
+#Print the result 
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print("+:", add, ", -:", sub, ", *:", mult, ", /:", div)
+
+#Calculate the modulus of two of the variables
+#Modulus is a operator that finds the remainder after division
+mod1 = int(input("Enter a number: "))
+mod2 = int(input("Enter another number: "))
+
+mod = mod1 % mod2
+
+print("% (remainder):", mod)
+
