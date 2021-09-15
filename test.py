74b2e47623fb7cd0dce775852c1d5f997f8c1cad
zsherry2106 <zsherry2106@gmail.com>
2021-09-15 09:10:59 -0500

    test page

--- test.py
+++ test.py
@@ -5,23 +5,7 @@
 
 
 #os clear does not work for me
-import os
-os.system('cls')
 
-userInput = input("Type something ")
+num = "0123456789"
 
-try:
-    int(userInput)
-    check = True
-#checks if there is a error, goes to except if there is
-except ValueError:
-    check = False
-
-
-if check:
-    print("Input is an integer")
-else:
-    print("Length of input:", len(userInput))
-    
-    for i in userInput:
-        print(i)
+print(num[0])
