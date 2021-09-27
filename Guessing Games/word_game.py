9fc30197eb808c25165245a9522d053db1ed1aae
zsherry2106 <zsherry2106@gmail.com>
2021-09-27 08:51:02 -0500

    word game

--- Guessing Games/word_game.py
+++ Guessing Games/word_game.py
@@ -6,17 +6,21 @@
 #os clear doens't work for me
 import os
 os.system('cls')
+print()
 
 import random
 
 fruits = ["strawberry", "pineapple", "grape", "apple", "kiwi"]
+hints =  ["this fruit is a berry", "this fruit goes on pizza", "this fruit is green or purple", "this fruit is red or green", "this fruit is fuzzy"]
 
 #print instructions for the game
 print("This is a guessing game. Try to guess the fruit I'm thinking of. You have 10 chances")
 playing = input("Do you want to play this game? ")
+print()
 
 while "y" in playing:
-    random_fruit = random.choice(fruits)
+    random_num = random.randint(0, len(fruits))
+    random_fruit = fruits[random_num]
     print(random_fruit)
 
     chances = 10
@@ -44,6 +48,10 @@ while "y" in playing:
             elif chances == 0:
                 print("You ran out of chances")
                 running = 0
+            
+            if chances == 3:
+                print("Here is a hint,")
+                print(hints[random_num])
         
         print()
     
