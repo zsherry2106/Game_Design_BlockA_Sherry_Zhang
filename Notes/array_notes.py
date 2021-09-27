26d3c2bca9c66e1dd314327238f7a9c2e78ad40c
zsherry2106 <zsherry2106@gmail.com>
2021-09-27 09:53:03 -0500

    array notes

---
+++ Notes/array_notes.py
@@ -0,0 +1,21 @@
+#Sherry Zhang
+#9/27/21
+
+#Notes on collections/arrays
+
+#list:
+print()
+
+myFruits = ["banana", "berries", "apple"]
+print(myFruits[0])
+print()
+
+for x in myFruits:
+    print(x)
+
+print()
+
+for x in myFruits[0]:
+    print(x)
+
+print()
