c702c50616fbec98390dec416200cdde8e1a6ffa
zsherry2106 <zsherry2106@gmail.com>
2021-09-29 21:41:01 -0500

    hangman

--- Guessing Games/hangman.py
+++ Guessing Games/hangman.py
@@ -11,8 +11,6 @@ import random
 animals = ["tiger", "lion", "elephant", "giraffe"]
 char_list = []
 
-chance_check = False
-
 print("In this game, you will guess letters in an animal's name one by one. You will only lose a chance if you guess incorrectly.")
 playing = input("Do you want to play this game? ")
 
@@ -20,11 +18,9 @@ playing = input("Do you want to play this game? ")
 while "y" in playing:
     random_anim = random.choice(animals)
     # print(random_anim)
-    running = 1
 
     #create character lists with _ to display guesses
-    for i in random_anim:
-        char_list.append("_ ")
+    char_list = ["_ "] * len(random_anim)
     
     chances = len(random_anim) + 3
     print(f"You have {chances} chances")
@@ -35,56 +31,43 @@ while "y" in playing:
     print(str_char)
 
     #while the player hasn't won or still has chances
-    while running and chances > 0:
+    while 1:
         guess = input("Guess a letter of an animal, type stop to end: ")
+        guess = guess.lower()
         print()
 
-        if guess.lower() != "stop":
-            #if input is an integer, subtract a guess and do not continue
-            try:
-                int(guess)
-                print("Please guess a letter")
+        if guess == "stop":
+            break
+
+        elif guess.isalpha() and len(guess) == 1:
+            #check if guess is in random word
+            if guess in random_anim:
+                for i in range(len(random_anim)):
+                    if random_anim[i] == guess:
+                        char_list[i] = guess
+                        str_char = "".join(char_list)
+            
+                #check if the player has won
+                if str_char == random_anim:
+                    print("You won!")
+                    break
+            
+            else:
                 chances -= 1
+                print("incorrect guess")
+                if chances == 0:
+                    print("Ran out of chances")
+                    break 
 
-            except ValueError:
-                #make sure guess is 1 letter, not multiple
-                if len(guess) < 2:
-
-                    #check if guess is in random word
-                    for i in range(len(random_anim)):
-                        if random_anim[i] == guess.lower():
-                            char_list[i] = f"{guess.lower()}"
-                            chance_check = True
-                            str_char = "".join(char_list)
-                    
-                    #create message that they guessed incorrectly
-                    if chance_check == False:
-                        chances -= 1
-                        print("incorrect guess")
-
-                    #check if the player has won
-                    if str_char == random_anim:
-                        print("You won!")
-                        running = 0
-                    
-                    #check chances to print why the player has lost
-                    if chances == 0:
-                        print("You have run out of chances")
-
-                    #print chances left
-                    elif running == 1:
-                        print("Chances left", chances)
-
-                #subtract guess if player guesses multiple char
-                else:
-                    print("Please guess a letter")
-                    chances -= 1
-
-                print(str_char)
         else:
-            running = 0
-        
-        chance_check = False
+            chances -= 1
+            print("Please guess a letter")
+            if chances == 0:
+                print("You ran out of guesses")
+                break
+
+        print("Chances left", chances)
+        print(str_char)
         
         print()
     
