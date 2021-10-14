9d02d58fdaec7b308ea63dcbc197473c8bcd8e50
zsherry2106 <zsherry2106@gmail.com>
2021-10-14 11:19:19 -0500

    hangman

--- Guessing Games/Hangman/hangman.py
+++ Guessing Games/Hangman/hangman.py
@@ -31,25 +31,18 @@ def Menu():
 
         selection = input("Which category would you like? ")
 
-        if selection.isdigit():
+        if selection.isdigit() and int(selection) <= 5:
             selection = int(selection)
-            if selection <= 5:
-                return selection
-            else:
-                print("Please enter a number between 1 and 4")
-        
+            return selection
         else:
-            print("Please enter a number")
+            print("Please enter a number between 1 and 4")
 
 #Uses selection from menu to select word from proper list
 def selectWord(selection):
-    # print(type(selection))
     if selection == 1:
         word = random.choice(animals)
-
     elif selection == 2:
         word = random.choice(fruits)
-    
     elif selection == 3:
         word = random.choice(colors)
 
@@ -62,75 +55,78 @@ high_score = []
 
 choice = Menu() #choose which category from menu
 
-#print scoreboard
-if choice == 4:
-    print("Scoreboard")
-    with open(os.path.join(this_folder, 'hangman.txt'), "r") as myfile:
-        print(myfile.read())
+while choice < 5 and playing_count < 3:
+    if choice != 4:
+        random_word = selectWord(choice)
+        # print(random_word)
+        score = 0
+        playing_count += 1
 
-while choice < 4 and playing_count < 3:
-    random_word = selectWord(choice)
-    # print(random_word)
-    score = 0
-    playing_count += 1
+        #create character lists with _ to display guesses
+        char_list = ["_ "] * len(random_word)
+        
+        chances = len(random_word) + 3
+        print(f"You have {chances} chances\n")
 
-    #create character lists with _ to display guesses
-    char_list = ["_ "] * len(random_word)
-    
-    chances = len(random_word) + 3
-    print(f"You have {chances} chances\n")
+        str_char = "".join(char_list)
 
-    str_char = "".join(char_list)
+        print(str_char)
 
-    print(str_char)
+        #while the player hasn't won or still has chances, continue, use break
+        while 1:
+            guess = input("Guess a letter of the word, type stop to end: ")
+            guess = guess.lower()
+            print()
 
-    #while the player hasn't won or still has chances
-    while 1:
-        guess = input("Guess a letter of the word, type stop to end: ")
-        guess = guess.lower()
-        print()
+            if guess == "stop":
+                break
 
-        if guess == "stop":
-            break
+            #make sure input is exactly one letter
+            elif guess.isalpha() and len(guess) == 1:
+                #check if guess is in random word
+                if guess in random_word and guess not in char_list:
+                    for i in range(len(random_word)):
+                        if random_word[i] == guess:
+                            char_list[i] = guess
+                            str_char = "".join(char_list)
+                            score += 3
+                
+                    #check if the player has won
+                    if str_char == random_word:
+                        print("You won!")
+                        score += 5*chances
+                        break
+                
+                #Subtract chance for repeat guesses
+                else:
+                    print("You already guessed that letter!")
+                    chances -= 1
 
-        elif guess.isalpha() and len(guess) == 1:
-            #check if guess is in random word
-            if guess in random_word and guess not in char_list:
-                for i in range(len(random_word)):
-                    if random_word[i] == guess:
-                        char_list[i] = guess
-                        str_char = "".join(char_list)
-                        score += 3
-            
-                #check if the player has won
-                if str_char == random_word:
-                    print("You won!")
-                    score += 5*chances
-                    break
-            
+            #if input is not one letter 
             else:
-                print("You already guessed that letter!")
+                print("Please guess a letter")
                 chances -= 1
-            
-        else:
-            print("Please guess a letter")
-            chances -= 1
 
-        if chances == 0:
-            print("You ran out of guesses")
-            break
+            #break if there are no chances left
+            if chances == 0:
+                print("You ran out of guesses")
+                break
 
-        print("Chances left", chances)
-        print(f"{str_char}\n")
+            print("Chances left", chances)
+            print(f"{str_char}\n")
+        
+        high_score.append(score)
+        
+        print("\nthe word was", random_word)
+        print("your score was", score)
+        char_list = []
     
-    high_score.append(score)
+    else:
+        print("\nScoreboard:")
+        with open(os.path.join(this_folder, 'hangman.txt'), "r") as myfile:
+            print(myfile.read())
     
-    print("\nthe word was", random_word)
-    print("your score was", score)
-    char_list = []
-
-    if playing_count != 3:
-        choice = Menu()
+    choice = Menu()
 
 #If player exits at the beginning, do not run this code
 if len(high_score) != 0:
@@ -141,7 +137,4 @@ if len(high_score) != 0:
     with open(os.path.join(this_folder, 'hangman.txt'), "a") as myfile:
         myfile.write(f"\n{name} \tHighest score:\t {dt_string} {max(high_score)}")
 
-    #Prints scoreboard at end
-    print("\nScoreboard:")
-    with open(os.path.join(this_folder, 'hangman.txt'), "r") as myfile:
-        print(myfile.read())
+
