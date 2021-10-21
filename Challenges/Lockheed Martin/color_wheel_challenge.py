789fa1bb4601b604367f4a9a507d83485df7741c
zsherry2106 <zsherry2106@gmail.com>
2021-10-21 09:20:38 -0500

    color wheel challenge

--- Challenges/Lockheed Martin/color_wheel_challenge.py
+++ Challenges/Lockheed Martin/color_wheel_challenge.py
@@ -11,27 +11,37 @@ mix = {'orange': ['yellow', 'red'],
        'violet': ['red', 'blue'],
        'green': ['blue', 'yellow']}
 
-color_input = 0
-while color_input != 'stop':
-    while 1:
-        color_input = (input("Enter a color you want: ")).lower()
+count = 0
+while 1:
+    cases = input("Enter test cases: ")
 
-        if color_input.isdigit():
-            print("Please enter a color")
-        
-        else:
-            break
+    if cases.isdigit():
+        cases = int(cases)
+        while count < cases:
+            count += 1
+            while 1:
+                color_input = (input("Enter a color you want: ")).lower()
 
-    if "-" in color_input:
-        color = color_input.split("-")
-        print(f"In order to make {color_input}, {color[0]} and {color[1]} must be mixed\n")
+                if color_input.isdigit():
+                    count += 1
+                    print("Please enter a color")
+                
+                else:
+                    break
 
-    elif color_input in primary:
-        print(f"No colors need to be mixed to make {color_input}\n")
+            if "-" in color_input:
+                color = color_input.split("-")
+                print(f"In order to make {color_input}, {color[0]} and {color[1]} must be mixed\n")
 
-    elif color_input in mix.keys():
-        color = mix.get(color_input)
-        print(f"In order to make {color_input}, {color[0]} and {color[1]} must be mixed\n")
+            elif color_input in primary:
+                print(f"No colors need to be mixed to make {color_input}\n")
 
-    else:
-        color = "Not a color"
+            elif color_input in mix.keys():
+                color = mix.get(color_input)
+                print(f"In order to make {color_input}, {color[0]} and {color[1]} must be mixed\n")
+
+            else:
+                color = "Not a color"
+    
+    elif cases.isalpha():
+        print("Please enter a number")
