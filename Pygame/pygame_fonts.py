9c8d45dacb0cb537924d59aae68df7d2f7bb5b0f
zsherry2106 <zsherry2106@gmail.com>
2021-10-26 12:29:39 -0500

    pygame fonts

--- Pygame/pygame_fonts.py
+++ Pygame/pygame_fonts.py
@@ -1,16 +1,46 @@
 #Sherry Zhang
 #10/25/21
 
-#Pygame fonts
+#pygame fonts
 
-import pygame as py, os
+import pygame as pygame, os
 os.system('clear')
 
-py.init()
+pygame.init()
 
-WIDTH, HEIGHT = 800
-window = py.display.set_mode((WIDTH, HEIGHT))
-py.display.set_captino("Setting Window")
+WIDTH, HEIGHT = 600, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Setting Window")
 
 #pygame font has 4 parameters: name, size, bold, and italic
-TITLE_FONT = py.font.SysFont("Arial", 12, bold = False, italic = False)
+#bold/italic automatically false
+TITLE_FONT = pygame.font.SysFont("Times New Roman", 80)
+SUBTITLE_FONT = pygame.font.SysFont("Times New Roman", 40)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+run = True
+
+def display_text(msg, height, font):
+    #takes text, thickness, color
+    # pygame.time.delay(1000)
+    text = font.render(msg, 5, BLACK)
+    window.blit(text, (WIDTH/2 - text.get_width()/2, height))
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    window.fill(WHITE)
+    
+    display_text("Settings", 10, TITLE_FONT)
+    display_text("Screen Size", 110, SUBTITLE_FONT)
+    display_text("Background Color", 160, SUBTITLE_FONT)
+    display_text("Object Color", 210, SUBTITLE_FONT)
+    display_text("Sound On/Off", 260, SUBTITLE_FONT)
+
+    pygame.display.flip()
+
+    pygame.display.flip()
